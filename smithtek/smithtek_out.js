1b04d9fdf5ae9166aa70479318895e68ee7c16d2
Jose Garcia <jose.reyes@ubidots.com>
2018-08-30 15:56:49 -0500

    modifying JS script to support simple-node mode on Output Smithtek node

--- smithtek/smithtek_out.js
+++ smithtek/smithtek_out.js
@@ -1,13 +1,19 @@
 module.exports = function (RED) {
     var mqtt = require("mqtt");
 
-    function postSmithTek(self, label_device, values, token) {
+    function postSmithTek(self, label_device, values, token, label_variable, simple_node) {
         var client = mqtt.connect('mqtt://industrial.api.ubidots.com', {username: token, password: ""});
+        var payload = values;
+
+        if (simple_node) {
+            payload = {[label_variable]:values};
+            payload = JSON.stringify(payload);
+        }
 
         client.on("connect", function () {
             client.publish(
                 "/v1.6/devices/" + label_device + "",
-                values,
+                payload,
                 {'qos': 1, 'retain': false},
                 function (error, response) {
                     client.end(true, function () {});
@@ -27,15 +33,17 @@ module.exports = function (RED) {
 
         this.on("input", function (msg) {
             var label_device = (msg.device_label || n.device_label) || (msg.label_device || n.label_device);
-            var values = (typeof msg.payload !== 'object' || msg.payload === null) ? {} : msg.payload;
+            var  values = (!['object', 'number', 'string'].includes(typeof msg.payload) || msg.payload === null) ? {} : msg.payload;
             var token = msg.token || n.token;
+            var label_variable = n.label_variable;
+            var simple_node = n.simple_node;
 
             if (typeof (values) === 'object') {
                 values = JSON.stringify(values);
             }
 
             self.status({ fill: "green", shape: "ring", text: "smithtek.connecting" });
-            postSmithTek(self, label_device, values, token);
+            postSmithTek(self, label_device, values, token, label_variable, simple_node);
         });
     }
 
